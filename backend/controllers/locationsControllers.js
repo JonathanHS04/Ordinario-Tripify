7dd0e324962d67f761b6c0607d2cf5270ac056cf
Jonathan Hillman <jonathanhillmansanchez@gmail.com>
2025-11-30 22:38:32 -0600

    cambios no importantes

---
+++ backend/controllers/locationsControllers.js
@@ -0,0 +1,15 @@
+const asyncHandler = require ('express-async-handler')
+
+const getLocations = asyncHandler( async(req, res) => {
+    
+})
+
+const addLocation = asyncHandler( async(req, res) => {
+    
+})
+
+
+module.exports = {
+    getLocations,
+    addLocation
+}
---
+++ backend/controllers/tasksControllers.js
@@ -0,0 +1,15 @@
+const asyncHandler = require('express-async-handler')
+
+const getTasks = asyncHandler( async(req, res) => {
+    
+})
+
+const addTask = asyncHandler( async(req, res) => {
+    
+})
+
+
+module.exports = {
+    getTasks,
+    addTask
+}
