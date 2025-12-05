9eb6fc02bff07437ca8aef451efc83f41ca81f6f
Jonathan Hillman <jonathanhillmansanchez@gmail.com>
2025-12-04 18:00:49 -0600

    cambio postgres

--- backend/config/postgresdb.js
+++ backend/config/postgresdb.js
@@ -1,12 +1,16 @@
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+  dialect: 'postgres',
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false },
     family: 4
   }
 });
 
-
 module.exports = sequelize;
+
 
